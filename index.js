8b20a72185c61690afea1b47b80c81abe0978dbc
sos161708fvl <djspine90@gmail.com>
2017-04-05 09:07:46 +0000

    API PACO

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var BASE_API_PATH = "/api/v1";
 var publicFolder = path.join(__dirname, 'public');
 
 var dbRoberto;
+var dbPaco;
 
 
 
@@ -28,7 +29,8 @@ MongoClient.connect(mdbURL,{native_parser:true}, function(err, database){
     
     
     dbRoberto = database.collection("provinces");
-    
+    dbPaco = database.collection("victims-stats");
+   
     
     app.listen(port, () => {
         console.log("Magic is happening on port " + port);
@@ -511,6 +513,484 @@ app.delete(BASE_API_PATH + "/provinces/:province/:year", function (request, resp
     } else {
         console.log("INFO: New DELETE request to /provinces/" + province + " and year " + year);
         dbRoberto.remove({province:province, $and:[{year:year}]}, {}, function (err, result) {
+            var numRemoved= JSON.parse(result);
+            if (err) {
+                console.error('WARNING: Error removing data from DB');
+                response.sendStatus(500); // internal server error
+            } else {
+                console.log("INFO: Results removed: " + numRemoved.n);
+                if (numRemoved.n === 1) {
+                    console.log("INFO: The result with province " + province + "and year " + year + " has been succesfully deleted, sending 204...");
+                    response.sendStatus(204); // not found
+                } else {
+                    console.log("WARNING: There are no provinces to delete");
+                    response.sendStatus(404); // no content
+                }
+            }
+        });
+    }}
+});
+
+
+
+
+//***********************************************************************************************************************************************************************************************************************
+//***************************************************************************************API PACO*********************************************************************************************************************
+//***********************************************************************************************************************************************************************************************************************
+
+
+
+///CREACIÓN DE LA APIKEY///
+
+var apiKeyPaco = "paco";
+
+//COMPROBANDo EL APIKEY
+function apiKeyCheck(request,response){
+    var ak = request.query.apikey;
+    var check = true;
+    
+    if(!ak){
+        console.log("WARNING: Necesita introducir una apikey para acceder a los datos. Aquí está su apikey: "+ apiKeyPaco);
+        check = false;
+        response.sendStatus(401);
+    }else{
+        if(ak != apiKeyPaco){
+            console.log("WARNING: La APIKEY introducida no es válida, aquí está la apikey válida "+ apiKeyPaco);
+            check=false;
+            response.sendStatus(403);
+        }
+    }
+    return check;
+}
+
+
+
+
+
+
+//Load Initial Data
+app.get(BASE_API_PATH + "/victims-stats/loadInitialData",function(request, response) {
+        if(apiKeyCheck(request,response)==true){
+
+    dbPaco.find({}).toArray(function(err,provinces){
+        
+         if (err) {
+        console.error('WARNING: Error while getting initial data from DB');
+        return 0;
+    }
+    
+      if (provinces.length === 0) {
+        console.log('INFO: Empty DB, loading initial data');
+
+       var province = [{
+                "province": "Seville",
+                "year": "2015",
+                "varied": "1,23%",
+                "averageWage": "15.921"
+            },
+            {
+                "province": "Madrid",
+                "year": "2014",
+                "varied": "0,66%",
+                "averageWage": "24.734"
+                
+            },
+            {
+                "province": "Barcelona",
+                "year": "2013",
+                "varied": "1,29%",
+                "averageWage": "22.153"
+            },
+            {
+                "province": "Valencia",
+                "year": "2012",
+                "varied": "1,35%",
+                "averageWage": "18.052"
+            }];
+        dbPaco.insert(province);
+        response.sendStatus(201) //created
+    } else {
+            
+
+        console.log('INFO: DB has ' + provinces.length + ' provinces ');
+            response.sendStatus(200) //created
+        
+    }
+});
+}
+});
+/*
+// GET a collection 
+ app.get(BASE_API_PATH + "/provinces", function(request, response) {
+  var url = request.query;
+  var province = url.province;
+  var year = url.year;
+  var varied = url.varied;
+  var averageWage = url.averageWage;
+  var offSet = 0;
+  var limit = 6;
+      if(apiKeyCheck(request,response)==true){
+
+ if (url.limit != undefined) {
+     limit = parseInt(url.limit);
+     offSet = parseInt(url.offset);
+    }
+    dbPaco.find({}).skip(offSet).limit(limit).toArray(function(err, asd) {
+     if (err) {
+      console.error('WARNING: Error getting data from DB');
+      response.sendStatus(500); // internal server error
+     }
+     else {
+      var filted = asd.filter((stat) => {
+       if ((province == undefined || stat.province == province) && (year == undefined || stat.year == year) && (varied == undefined || stat.varied == varied) && (averageWage == undefined || stat.averageWage == averageWage)) {
+        return stat;
+       }
+      });
+      if (filted.length > 0) {
+       console.log("INFO: Sending stat: " + JSON.stringify(filted, 2, null));
+       response.send(filted);
+      }
+      else {
+       console.log("WARNING: There are not any province with this properties");
+       response.sendStatus(404); // not found
+      }
+     }
+    });
+      }
+  });
+
+*/
+  
+ // GET Collection (WITH SEARCH)
+
+app.get(BASE_API_PATH + "/provinces", function (request, response) {
+    if (!apiKeyCheck(request, response)) return;
+    console.log("INFO: New GET request to /provinces");
+    
+            var limit = parseInt(request.query.limit);
+            var offset = parseInt(request.query.offset);
+            var from = request.query.from;
+            var to = request.query.to;
+            var aux = [];
+            var aux2= [];
+
+            
+            if (limit && offset >=0) {
+            dbPaco.find({}).skip(offset).limit(limit).toArray(function(err, provinces) {
+                if (err) {
+                    console.error('WARNING: Error getting data from DB');
+                     response.sendStatus(500); // internal server error
+                } else {
+                     if (provinces.length === 0) {
+                            response.sendStatus(404);
+                        }
+                    console.log("INFO: Sending provinces: " + JSON.stringify(provinces, 2, null));
+                    if (from && to) {
+
+                            aux = buscador(provinces, aux, from, to);
+                            if (aux.length > 0) {
+                                aux2 = aux.slice(offset, offset+limit);
+                                console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux, 2, null));
+                                console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(provinces, 2, null));
+                                console.log("INFO: Sending results with from and to and limit and offset: " + JSON.stringify(aux2, 2, null));
+                                response.send(aux2);
+                            }
+                            else {
+                                response.sendStatus(404); // No content 
+                            }
+                        }
+                        else {
+                            response.send(provinces);
+                        }
+                }
+            });
+            
+            }
+            else {
+
+                dbPaco.find({}).toArray(function(err, provinces) {
+                    if (err) {
+                        console.error('ERROR from database');
+                        response.sendStatus(500); // internal server error
+                    }
+                    else {
+                        if (provinces.length === 0) {
+                            response.sendStatus(404);
+                        }
+                        console.log("INFO: Sending provinces: " + JSON.stringify(provinces, 2, null));
+                        if (from && to) {
+                            aux = buscador(provinces, aux, from, to);
+                            if (aux.length > 0) {
+                                response.send(aux);
+                            }
+                            else {
+                                response.sendStatus(404); //No content
+                            }
+                        }
+                        else {
+                            response.send(provinces);
+                        }
+                    }
+                });
+            }
+
+});
+
+
+
+
+  
+// SEARCH FUNCTION
+
+var buscador = function(base, conjuntoauxiliar, desde, hasta) {
+
+    var from = parseInt(desde);
+    var to = parseInt(hasta);
+
+
+    for (var j = 0; j < base.length; j++) {
+        var anio = base[j].year;
+        if (to >= anio && from <= anio) {
+
+            conjuntoauxiliar.push(base[j]);
+        }
+    }
+
+    return conjuntoauxiliar;
+
+};
+
+
+/*
+// GET a collection
+app.get(BASE_API_PATH + "/provinces", function (request, response) {
+        if(apiKeyCheck(request,response)==true){
+
+    console.log("INFO: New GET request to /provinces");
+    dbPaco.find({}).toArray( function (err, provinces) {
+        if (err) {
+            console.error('WARNING: Error getting data from DB');
+            response.sendStatus(500); // internal server error
+        } else {
+            console.log("INFO: Sending provinces: " + JSON.stringify(provinces, 2, null));
+            response.send(provinces);
+        }
+    });
+        }
+});
+
+*/
+// GET a collection de paises en un mismo año 
+
+app.get(BASE_API_PATH + "/provinces/:year", function (request, response) {
+        if(apiKeyCheck(request,response)==true){
+
+    var year = request.params.year;
+    var province = request.params.year;
+    if(isNaN(request.params.year.charAt(0))){
+            if (!province) {
+        console.log("WARNING: New GET request to /provinces/:province without name, sending 400...");
+        response.sendStatus(400); // bad request
+    } else {
+        console.log("INFO: New GET request to /provices/" + province);
+        dbPaco.find({province:province}).toArray(function (err, results) {
+            if (err) {
+                console.error('WARNING: Error getting data from DB');
+                response.sendStatus(500); // internal server error
+            } else if (results.length > 0) { 
+                    var result = results; //since we expect to have exactly ONE contact with this name
+                    console.log("INFO: Sending result: " + JSON.stringify(result, 2, null));
+                    response.send(result);
+                } else {
+                    console.log("WARNING: There are not any result with province " + province);
+                    response.sendStatus(404); // not found
+                }
+        });
+}
+    }else{
+    if (!year) {
+        console.log("WARNING: New GET request to /provinces/:year without year, sending 400...");
+        response.sendStatus(400); // bad request
+    } else {
+        console.log("INFO: New GET request to /provinces/" + year);
+        dbPaco.find({year:year}).toArray(function (err, results) {
+            if (err) {
+                console.error('WARNING: Error getting data from DB');
+                response.sendStatus(500); // internal server error
+            } else if (results.length > 0) { 
+                    var result = results; //since we expect to have exactly ONE contact with this name
+                    console.log("INFO: Sending result: " + JSON.stringify(result, 2, null));
+                    response.send(result);
+                } else {
+                    console.log("WARNING: There are not any result with year " + year);
+                    response.sendStatus(404); // not found
+                
+                }
+        });
+}}
+}});
+
+
+//GET a recurso concreto con 2 parametros
+
+app.get(BASE_API_PATH + "/provinces/:province/:year", function (request, response) {
+    var province = request.params.province;
+    var year = request.params.year;
+        if(apiKeyCheck(request,response)==true){
+
+    if (!province || !year) {
+        console.log("WARNING: New GET request to /provinces/:province without name or without year, sending 400...");
+        response.sendStatus(400); // bad request
+    } else {
+        console.log("INFO: New GET request to /provinces/" + province + "/" + year);
+        dbPaco.find({province:province, $and:[{year:year}]}).toArray(function (err, results) {
+            if (err) {
+                console.error('WARNING: Error getting data from DB');
+                response.sendStatus(500); // internal server error
+            } else if (results.length > 0) { 
+                    var result = results[0]; //since we expect to have exactly ONE contact with this name
+                    console.log("INFO: Sending result: " + JSON.stringify(result, 2, null));
+                    response.send(result);
+                } else {
+                    console.log("WARNING: There are not any province with name " + province +  "and year " + year);
+                    response.sendStatus(404); // not found
+                
+                }
+        });
+}}
+});
+
+
+//POST over a collection
+app.post(BASE_API_PATH + "/provinces", function (request, response) {
+    var newstat = request.body;
+        if(apiKeyCheck(request,response)==true){
+
+    if (!newstat) {
+        console.log("WARNING: New POST request to /provinces/ without stat, sending 400...");
+        response.sendStatus(400); // bad request
+    } else {
+        console.log("INFO: New POST request to /provinces with body: " + JSON.stringify(newstat, 2, null));
+        if (!newstat.province || !newstat.year ||  !newstat.varied || !newstat.averageWage) {
+            console.log("WARNING: The stat " + JSON.stringify(newstat, 2, null) + " is not well-formed, sending 422...");
+            response.sendStatus(422); // unprocessable entity
+        } else {
+            dbPaco.find({}).toArray( function (err, provinces) {
+                if (err) {
+                    console.error('WARNING: Error getting data from DB');
+                    response.sendStatus(500); // internal server error
+                } else {
+                    var provincesBeforeInsertion = provinces.filter((stats) => {
+                        return (stats.province.localeCompare(newstat.province, "en", {'sensitivity': 'base'}) === 0);
+                    });
+                    if (provincesBeforeInsertion.length > 0) {
+                        console.log("WARNING: The stat " + JSON.stringify(newstat, 2, null) + " already extis, sending 409...");
+                        response.sendStatus(409); // conflict
+                    } else {
+                        console.log("INFO: Adding stat " + JSON.stringify(newstat, 2, null));
+                        dbPaco.insert(newstat);
+                        response.sendStatus(201); // created
+                    }
+                }
+            });
+        }
+    }}
+});
+
+//a
+
+//POST over a single resource NO PERMITIDO
+app.post(BASE_API_PATH + "/provinces/:province", function (request, response) {
+    var province = request.params.province;
+        if(apiKeyCheck(request,response)==true){
+
+    console.log("WARNING: New POST request to /provinces/" + province + ", sending 405...");
+    response.sendStatus(405); // method not allowed
+        }
+});
+
+
+//PUT over a collection NO PERMITIDO
+app.put(BASE_API_PATH + "/provinces", function (request, response) {
+        if(apiKeyCheck(request,response)==true){
+
+    console.log("WARNING: New PUT request to /provinces, sending 405...");
+    response.sendStatus(405); // method not allowed
+        }
+});
+
+
+//PUT over a single resource
+app.put(BASE_API_PATH + "/provinces/:province/:year", function (request, response) {
+    var updatedStat = request.body;
+    var province = request.params.province;
+    var year = request.params.year;
+        if(apiKeyCheck(request,response)==true){
+
+
+    if (!updatedStat) {
+        console.log("WARNING: New PUT request to /provinces/ without stat, sending 400...");
+        response.sendStatus(400); // bad request
+    } else {
+        console.log("INFO: New PUT request to /provinces/" + province + " with data " + JSON.stringify(updatedStat, 2, null));
+        if (!updatedStat.province || !updatedStat.year ||  !updatedStat.varied || !updatedStat.averageWage) {
+            console.log("WARNING: The stat " + JSON.stringify(updatedStat, 2, null) + " is not well-formed, sending 422...");
+            response.sendStatus(422); // unprocessable entity
+        } else {
+            dbPaco.find({province:province, $and:[{year:year}]}).toArray( function (err, provinces) {
+                if (err) {
+                    console.error('WARNING: Error getting data from DB');
+                    response.sendStatus(500); // internal server error
+                } else if (provinces.length > 0) {
+                        dbPaco.update({province: province, year: year}, updatedStat);
+                        console.log("INFO: Modifying result with proviç " + province + " with data " + JSON.stringify(updatedStat, 2, null));
+                        response.send(updatedStat); // return the updated contact
+                    } else {
+                        console.log("WARNING: There are not any result with province " + province);
+                        response.sendStatus(404); // not found
+                    }
+                }
+            )}
+        }}
+    });
+           
+           
+//DELETE over a collection
+app.delete(BASE_API_PATH + "/provinces", function (request, response) {
+        if(apiKeyCheck(request,response)==true){
+
+    console.log("INFO: New DELETE request to /provinces");
+    dbPaco.remove({}, {multi: true}, function (err, numRemoved) {
+        if (err) {
+            console.error('WARNING: Error removing data from DB');
+            response.sendStatus(500); // internal server error
+        } else {
+            if (numRemoved > 0) {
+                console.log("WARNING: There are no provinces to delete");
+                response.sendStatus(404); // no content
+            } else {
+                console.log("INFO: All the provinces (" + numRemoved + ") have been succesfully deleted, sending 204...");
+                response.sendStatus(204); // not found
+            }
+        }
+    });
+        }
+});
+
+
+
+//DELETE over a single resource
+app.delete(BASE_API_PATH + "/provinces/:province/:year", function (request, response) {
+    var province = request.params.province;
+    var year = request.params.year;
+        if(apiKeyCheck(request,response)==true){
+
+    if (!province || !year) {
+        console.log("WARNING: New DELETE request to /provinces/:province/:year without province and year, sending 400...");
+        response.sendStatus(400); // bad request
+    } else {
+        console.log("INFO: New DELETE request to /provinces/" + province + " and year " + year);
+        dbPaco.remove({province:province, $and:[{year:year}]}, {}, function (err, result) {
             var numRemoved= JSON.parse(result);
             if (err) {
                 console.error('WARNING: Error removing data from DB');
