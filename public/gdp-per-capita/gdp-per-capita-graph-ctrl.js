315b0a6292811b1731954c16e84be83d3b35dd00
sos161708rmn <r.mantas.nakhai@gmail.com>
2017-05-10 05:41:43 +0000

    feedBack F09 a herokugit add .

--- public/gdp-per-capita/gdp-per-capita-graph-ctrl.js
+++
@@ -1,8 +0,0 @@
-/* global angular */
-
-angular.module("SOS08ManagerApp").
-controller("GdpPerCapitaGraphCtrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope) {
-    console.log("Controller initialized (GdpPerCapitaGraphCtrl)");
-    
-    if (!$rootScope.apikey) $rootScope.apikey = "secret";
-}]);
